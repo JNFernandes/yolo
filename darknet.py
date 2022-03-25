ae986a75105416c06ae3d57d1196edc034a8743c
JNFernandes <joaofernandes2080@gmail.com>
2022-03-25 16:21:11 +0000

    function parsing cfg defined

--- darknet.py
+++ darknet.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
 
+
 def parse_cfg(cfgfile):
     """
     Takes a configuration file
@@ -14,3 +15,25 @@ def parse_cfg(cfgfile):
     network to be built. Block is represented as a dictionary in the list
 
     """
+
+    file = open(cfgfile, 'r')
+    lines = file.read().split('\n')  # store the lines in a list
+    lines = [i for i in lines if len(i) > 0]  # get rid of the empty lines
+    lines = [i for i in lines if i[0] != "#"]   # get rid of comments
+    lines = [i.rstrip().lstrip() for i in lines]  # get rid of whitespaces
+
+    block = {}
+    blocks = []
+
+    for line in lines:
+        if line[0] == "[":  # This marks the start of a new block
+            if (len(block) != 0):  # If block is not empty, implies it is storing values of previous block
+                blocks.append(block)  # add it the blocks list
+                block = {}  # re-init the block
+            block["type"] = line[1:-1].rstrip()
+        else:
+            key, value = line.split("=")
+            block[key.rstrip()] = value.lstrip()
+    blocks.append(block)
+
+    return blocks
